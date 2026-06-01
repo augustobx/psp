c8f93559ba48a0ee2ad4df25cb62046ae334a7d2
augustobx <augustobasquez@gmail.com>
2026-06-01 00:29:58 -0300

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export default function proxy(request: NextRequest) {
   // Solo interceptamos rutas de /admin
   if (request.nextUrl.pathname.startsWith('/admin')) {
     const authCookie = request.cookies.get('admin_auth');
